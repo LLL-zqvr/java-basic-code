c95bbf1f0571ae28e5a8e599b88305badf37a206
LLL-zvqr <13657794496@163.com>
2024-03-23 22:22:06 +0800

    init

--- day07/src/main/java/com/itheima/test2/GirlFriend.java
+++ day07/src/main/java/com/itheima/test2/GirlFriend.java
@@ -2,9 +2,32 @@ package com.itheima.test2;
 
 public class GirlFriend {
     //属性
-    String name;
-    int age;
-    String gender;
+   private String name;
+   private int age;
+   private String gender;
+   //name的set,给成员赋值
+    public void setName(String n){
+        name = n;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int n){
+        if(n >=18 && n <=28){
+            age = n;
+        }else{
+            System.out.println("非法数据");
+        }
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setGender(String n){
+        gender = n;
+    }
+    public String getGender(){
+        return gender;
+    }
     //行为
     public void sleep(){
         System.out.println("女朋友在睡觉");
--- day07/src/main/java/com/itheima/test2/GirlFriendTest.java
+++ day07/src/main/java/com/itheima/test2/GirlFriendTest.java
@@ -4,12 +4,26 @@ public class GirlFriendTest {
     public static void main(String[] args) {
         //创建女朋友对象
         GirlFriend gf1 = new GirlFriend();
-        gf1.name = "小诗诗";
-        gf1.age = 18;
-        gf1.gender = "萌妹子";
-        System.out.println(gf1.name);
-        System.out.println(gf1.age);
-        System.out.println(gf1.gender);
+        gf1.setName("小诗诗");
+        gf1.setAge(18);
+        gf1.setGender("萌妹子");
+        System.out.println(gf1.getName());
+        System.out.println(gf1.getAge());
+        System.out.println(gf1.getGender());
+        gf1.eat();
+        gf1.sleep();
+        System.out.println("===================================");
+        GirlFriend gf2 = new GirlFriend();
+        gf2.setName("小澹澹");
+        gf2.setAge(18);
+        gf2.setGender("傲娇妹子");
+        System.out.println(gf2.getName());
+        System.out.println(gf2.getAge());
+        System.out.println(gf2.getGender());
+        gf2.eat();
+        gf2.sleep();
+
+
 
     }
 }
