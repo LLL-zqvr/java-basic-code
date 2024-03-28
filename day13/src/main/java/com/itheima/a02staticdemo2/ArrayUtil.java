1145f6a0a772b90d879cc9cff4bdeb84bcd50336
LLL-zvqr <13657794496@163.com>
2024-03-28 22:53:43 +0800

    init

---
+++ day13/src/main/java/com/itheima/a02staticdemo2/ArrayUtil.java
@@ -0,0 +1,12 @@
+package com.itheima.a02staticdemo2;
+
+public class ArrayUtil {
+    //私有化构造方法
+    private ArrayUtil(){}
+    //目的：为了不让外界创建它的对象
+    //需要定义为静态的，方便调用
+    public static String printArr(int[] arr){
+        StringBuilder sb = new StringBuilder();
+        
+    }
+}
